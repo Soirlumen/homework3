730d8e8cc5ed0d0ef656244e714286b3b70fc890
unknown <novot129@fjfi.cvut.cz>
2024-11-23 20:53:42 +0100

    security and stuff its just right except for the excepts

---
+++ Eratosthen_/functions.cpp
@@ -0,0 +1,80 @@
+ #include "header.h"
+
+
+int vstup(){
+    std::string l;
+    vypiš("Zadej cele kladne cislo: ");
+    std::cin >> l;
+    try {
+        int b = std::stoi(l);
+        if (b > 0) {
+            return b;
+        } else {
+            vypiš("zadane cislo '",b,"' neni kladne.\n");
+        }
+    } catch (std::out_of_range&) {
+        vypiš("cislo moc velke.\n");
+    }
+    catch (std::invalid_argument){
+        vypiš("zadana vec neplatna.\n");
+    }
+    return -1;
+}
+
+bool je_dělitelné(int i, int j){
+    return(((i%j)==0)?true:false); 
+}
+
+void vypištab(bool tabule[], int velikost_tabulky){
+    std::cout<<"vypisuju prvocisla od 2 do "<<velikost_tabulky-1<<std::endl;
+    int h=0;
+    for (int k=0;k<=velikost_tabulky;k++){
+        if (tabule[k]){
+            std::cout<<k<<std::endl;
+            h++;
+        }
+    }
+    std::cout<<"pocet vsech prvocisel: "<<h<<std::endl;
+}
+
+void hledam_prvocislo(bool tabule[], int l, int velikost_tabulky)
+{
+    int ctverec=l*l;
+    for(int i=ctverec;i<=velikost_tabulky;i++){
+        if (je_dělitelné(i, l)){
+            tabule[i]=false;
+            //std::cout<<"cislo "<<i<<" je delitelne " <<l<<std::endl;
+        }
+    }
+}
+
+void init(bool tabule[])
+{
+    tabule[0]=false;
+    tabule[1]=false;
+}
+
+void eratosyto()
+{
+    int velikostTabulky = vstup()+1;
+    switch (velikostTabulky)
+    {
+    case -1:
+        vypiš("koncim program papa.");
+        break;
+    case 1:
+        vypiš("nebudu delat tabulku s velikosti ", velikostTabulky, ", koncim program papa.");
+        break;
+    
+    default:
+        bool * pole = new bool [velikostTabulky];
+        std::fill_n(pole, velikostTabulky, 1);
+        init(pole);
+        for (int j=2;j<=velikostTabulky;j++)
+        {
+            hledam_prvocislo(pole,j, velikostTabulky);
+        }
+        vypištab(pole,velikostTabulky);
+        delete [] pole;
+    }
+}
--- Eratosthen_/main.cpp
+++ Eratosthen_/main.cpp
@@ -1,60 +1,11 @@
-#include <iostream>
+
 #include "header.h"
 //prvni messy verze eratosthena který neni bezpecny a je strasne stinky
 
-const int velikost_tabulky=33;
-bool tabulka[velikost_tabulky];
-
-void tabulec(bool tabulka[])
-{for(int m=0;m<velikost_tabulky;m++){
-    tabulka[m]=true;
-}}
-
-void init1(bool tabule[]){
-    tabule[0]=false;
-    tabule[1]=false;
-}
-bool je_dělitelné(int i, int j){
-    return(((i%j)==0)?true:false); 
-}
-
-void hledam_prvocislo(bool tabule[], int l){
-    int ctverec=l*l;
-    for(int i=ctverec;i<velikost_tabulky;i++){
-        if (je_dělitelné(i, l)){
-            tabule[i]=false;
-            std::cout<<"cislo "<<i<<" je delitelne " <<l<<std::endl;
-        }
-    }
-}
-void vypiš(bool tabule[]){
-    std::cout<<"vypisuju prvocisla od 2 do "<<velikost_tabulky<<std::endl;
-    int h=0;
-    for (int k=0;k<velikost_tabulky;k++){
-        if (tabule[k]){
-            std::cout<<k<<std::endl;
-            h++;
-        }
-    }
-    std::cout<<"pocet vsech prvocisel: "<<h<<std::endl;
-}
-void škrtám_prvnočísla(bool tabule[]){
-init1(tabule);
-for (int j=2;j<velikost_tabulky;j++){
-    hledam_prvocislo(tabule,j);
-}
-vypiš(tabule);
-}
 
 int main()
 {
 
-   //int k=3;
-   //int b=10;
-   //bool mnam=je_dělitelné(k,b);
-   //std::cout<<mnam;
-    tabulec(tabulka);
-    škrtám_prvnočísla(tabulka);
-
+    eratosyto();
     return 0;
 }
